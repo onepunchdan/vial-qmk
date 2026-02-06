1456206510a84dd96f9f58405b954d1e11af27c2
Dan Guevarra <onepunchdan@users.noreply.github.com>
2026-02-05 21:22:41 -0800

    fix: just use palettefx effects

--- compkeebs.sh
+++ compkeebs.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-qmk compile -c -kb hypharf/rk61 -km vial && qmk compile -kb keychron/v4/ansi -km vial && qmk compile -kb keychron/v3/ansi_encoder -km vial && qmk compile -kb keychron/v8/ansi_encoder -km vial && qmk compile -kb keychron/q3/ansi_encoder -km vial && qmk compile -kb keychron/q8/ansi_encoder -km vial
+qmk compile -c -kb hypharf/rk61 -km vial && qmk compile -kb keychron/v4/ansi -km vial && qmk compile -kb keychron/v3/ansi_encoder -km vial && qmk compile -kb keychron/v8/ansi_encoder -km vial && qmk compile -kb keychron/q3/ansi_encoder -km vial && qmk compile -kb keychron/q8/ansi_encoder -km vial && qmk compile -kb nuphy/air75_v2/ansi -km vial
